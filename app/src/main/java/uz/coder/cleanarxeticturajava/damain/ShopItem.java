23161e82d0e0bf7a6b0782a6b6deae10b911224a
Fozilbek0109 <strike0109k@gmail.com>
2023-09-07 11:59:30 +0500

    Initial commit

---
+++ app/src/main/java/uz/coder/cleanarxeticturajava/damain/ShopItem.java
@@ -0,0 +1,11 @@
+package uz.coder.cleanarxeticturajava.damain;
+
+public class ShopItem {
+    private String name;
+    private int count;
+    private int id =0;
+
+
+
+
+}
---
+++ app/src/main/java/uz/coder/cleanarxeticturajava/prasentation/MainActivity.java
@@ -0,0 +1,16 @@
+package uz.coder.cleanarxeticturajava.prasentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uz.coder.cleanarxeticturajava.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
